e10afc594ac995a23e05a908cb31a138bdb03966
Nick Jacobson <33440907+nick-jac@users.noreply.github.com>
2017-12-12 14:43:05 +1300

    Update eloquent-2.js

--- eloquent-2.js
+++ eloquent-2.js
@@ -1,1 +1,53 @@
+Looping a triangle
+Write a loop that makes seven calls to console.log to output the following triangle:
+#
+##
+###
+####
+#####
+######
+#######
+It may be useful to know that you can find the length of a string by writing .length after it.
+for (var line = "#"; line.length < 8; line += "#")
+  console.log(line);
+
+
+FizzBuzz
+Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.
+When you have that working, modify your program to print "FizzBuzz", for numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
+(This is actually an interview question that has been claimed to weed out a significant percentage of programmer candidates. So if you solved it, you’re now allowed to feel good about yourself.)
+for (var n = 1; n <= 100; n++) {
+var output = "";
+  if (n % 3 == 0) output += "Fizz";
+  if (n % 5 == 0) output += "Buzz";
+  console.log(output || n);
+}
+
+Chess board
+Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. At each position of the grid there is either a space or a “#” character. The characters should form a chess board.
+Passing this string to console.log should show something like this:
+ # # # #
+# # # #
+ # # # #
+# # # #
+ # # # #
+# # # #
+ # # # #
+# # # #
+When you have a program that generates this pattern, define a variable size = 8 and change the program so that it works for any size, outputting a grid of the given width and height.
+
+var size = 8; 
+var board = "";
+for (var y = 0; y < size; y++) {
+  for (var x = 0; x < size; x++) {
+    if ((x + y) % 2 == 0)
+     board += " ";
+   else 
+     board += "#";
+}
+  board += "\n";
+}
+console.log(board);
+
+
 
